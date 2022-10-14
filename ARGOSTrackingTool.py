7426664057976969b0a7f93c6be3840adecdb0b7
AKolarova <112417946+AKolarova@users.noreply.github.com>
2022-10-14 18:52:14 -0400

    Update ARGOSTrackingTool.py

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,17 +9,31 @@
 # Date:   Fall 2022
 #--------------------------------------------------------------
 
-#Create a variable poiting to the data file
+#Create a variable pointing to the data file
 file_name = 'data/raw/Sara.txt'
 
 #Create a file object from the file name
 file_object = open(file=file_name, mode='r')
 
-
 #Read contents of file into a list
 line_list = file_object.readlines()
 
+#Close the file object
+file_object.close()
+
+#Extract one data line into a variable
+lineString = line_list[200]
+
+# Split lineString into a list of items
+lineData = lineString.split()
+
+#Assign variables to items in the lineData list
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
 
 #Print information to the user
-#print(f'Record {record_id} indicates Sara was seen at {obs_lat}N.{obs_lon}W on {obs_date}.')
+print(f'Record {record_id} indicates Sara was seen at {obs_lat}N.{obs_lon}W on {obs_date}.')
 
